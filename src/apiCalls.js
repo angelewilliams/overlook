b40efc0f586b571e2e1e7accf9ebe7aa728d2b50
Angele Williams <angele.williams5@gmail.com>
2022-04-23 15:11:50 -0600

    Start connecting to DOM

---
+++ src/apiCalls.js
@@ -0,0 +1,33 @@
+// import { displayMessage } from './dom-updates.js'
+
+const getData = (url) => {
+  return fetch(`http://localhost:3001/api/v1/${url}`)
+     .then(response => response.json())
+     .then(data => data[url])
+     .catch(error => {
+       message.innerText = `There was a problem completing your request. Error: ${error.message}`
+       // displayMessage();
+     });
+}
+
+const postBooking = (booking) => {
+  return fetch(`http://localhost:3001/api/v1/booking`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(booking)
+  })
+    .then(response => {
+      if(!response.ok) throw new Error('Please fill out all fields.');
+      // message.innerText = `Your booking request has been accepted.`
+      // return response.json();
+    })
+    .catch(error => {
+      message.innerText = `Your Booking request has been rejected: ${error.message}`
+      // displayMessage();
+    });
+}
+
+export {
+  getData,
+  postBooking
+};
--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,38 @@
-// Do not delete or rename this file ********
+//♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢ Imports ♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢//
 import './css/styles.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 import './images/overlook-logo.png'
 // This is the JavaScript entry file - your code begins here
 
+//♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢Query Selectors♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢//
+const myReservations = document.querySelector(".reservations");
+const bookRoom = document.querySelector(".plan-booking-area");
 
+const upcomingBookings = document.querySelector(".upcoming-bookings");
+const upcomingBookingsArea = document.querySelector(".upcoming-bookings-area");
 
-console.log('This is the JavaScript entry file - your code begins here.');
+const pastBookings = document.querySelector(".past-bookings");
+const pastBookingsArea = document.querySelector(".past-bookings-area");
+
+const currentBookings = document.querySelector(".current-bookings");
+const currentBookingsArea = document.querySelector(".current-bookings-area");
+
+const homeButton = document.querySelector(".home");
+const bookTravelButton = document.querySelector(".book-travel-button");
+const calculateTravelButton = document.querySelector(".calculate-cost-button");
+const loginSubmitButton = document.querySelector(".submit-button");
+
+const startDate = document.querySelector(".date-picker");
+const estimatedTripCost = document.querySelector(".estimated-trip-cost");
+const bookTripForm = document.querySelector(".book-trip-form");
+
+const userView = document.querySelector(".user-view");
+const loginPage = document.querySelector(".login-page");
+const navbar = document.querySelector(".nav-bar");
+
+// const usernameInput = document.querySelector(".username");
+// const passwordInput = document.querySelector(".password");
 
 //♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢Event Listeners♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢//
 // window.onload()
